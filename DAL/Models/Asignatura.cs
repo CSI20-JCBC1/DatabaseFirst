41f20bd0fceac4dab9e177ba88a43f3b5b0acf0e
CSI20-JCBC1 <csi20-jbadrbo@colegioaltair.net>
2022-11-03 11:53:45 +0100

    Agregar archivos de proyecto.

---
+++ DAL/Models/Asignatura.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DAL.Models
+{
+    public partial class Asignatura
+    {
+        public Asignatura()
+        {
+            ListaAlumnos = new HashSet<Alumno>();
+        }
+
+        public int Id { get; set; }
+        public string AsignaturaNombre { get; set; } = null!;
+
+        public virtual ICollection<Alumno> ListaAlumnos { get; set; }
+    }
+}
